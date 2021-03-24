37f3fe3703df88e3538b6deb2c3c758a8c14f20f
Miguel Rosa <miguelrosa.programacao@gmail.com>
2021-03-24 11:58:54 +0000

    add plugins

---
+++ projects/twam2020/precache-manifest.231a3e7ee0c80dc5779da85d3f93ed5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8884193a442bd5343071f692966e8312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfa9eaf8512d7ab2ed8b",
+    "url": "/static/css/2.74f4c41c.chunk.css"
+  },
+  {
+    "revision": "e1bd8ae506a5f454140e",
+    "url": "/static/css/main.cad26a24.chunk.css"
+  },
+  {
+    "revision": "dfa9eaf8512d7ab2ed8b",
+    "url": "/static/js/2.31f887fe.chunk.js"
+  },
+  {
+    "revision": "a8f962b213a67202a0ddc78ab1211cdb",
+    "url": "/static/js/2.31f887fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1bd8ae506a5f454140e",
+    "url": "/static/js/main.e8af8e05.chunk.js"
+  },
+  {
+    "revision": "54cd28e502a929b708d2",
+    "url": "/static/js/runtime-main.ecbba5db.js"
+  }
+]);
