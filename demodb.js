0f04b369a8079c4ef001dcd3c9b2411f5dbcfc9b
Miguel Rosa <46021063+miguelnunorosa@users.noreply.github.com>
2024-04-02 08:50:38 +0000

    update

--- demodb.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require("sqlite3");
-const demoDB = new sqlite3.Database("./databases/demo.db");
-
-
-demoDB.all(`SELECT * FROM teste`, (err, info) =>{
-    console.log(err, info)
-});
