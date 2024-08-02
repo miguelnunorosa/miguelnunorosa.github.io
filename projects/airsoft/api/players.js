29de422ef98465e9807eda490c4581c8eca71e68
Miguel Rosa <46021063+miguelnunorosa@users.noreply.github.com>
2024-08-02 13:57:32 +0000

    update

---
+++ projects/airsoft/api/players.js
@@ -0,0 +1,49 @@
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_AUTH_DOMAIN",
+    projectId: "SEU_PROJECT_ID",
+    storageBucket: "SEU_STORAGE_BUCKET",
+    messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+    appId: "SEU_APP_ID",
+    measurementId: "SEU_MEASUREMENT_ID"
+};
+
+
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+
+
+
+// Função para buscar jogadores da coleção "players" e exibir na tabela
+async function fetchAndDisplayPlayers() {
+    try {
+        const playersTableBody = document.getElementById('players-table-body');
+        playersTableBody.innerHTML = ''; // Limpar a tabela antes de adicionar novos dados
+
+        const snapshot = await db.collection('players').get();
+        snapshot.forEach(doc => {
+            const playerData = doc.data();
+            const playerName = playerData.playerName;
+            const numberOfKills = playerData.numberOfKills || 0;
+            const numberOfDeaths = playerData.numberOfDeaths || 0;
+            const ratio = numberOfDeaths === 0 ? numberOfKills : (numberOfKills / numberOfDeaths).toFixed(2);
+
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${playerName}</td>
+                <td>${numberOfKills}</td>
+                <td>${numberOfDeaths}</td>
+                <td>${ratio}</td>
+            `;
+            playersTableBody.appendChild(row);
+        });
+    } catch (error) {
+        console.error('Erro ao buscar jogadores: ', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', fetchAndDisplayPlayers);
