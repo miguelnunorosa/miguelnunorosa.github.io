ddfa167e1d82db23bb72eb42b843509d577e0cbd
Miguel Rosa <46021063+miguelnunorosa@users.noreply.github.com>
2024-08-02 22:23:46 +0000

    new api version start

---
+++ projects/airsoft/api/games/theduel.js
@@ -0,0 +1,12 @@
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCyj-d_dhgMXHqKkG0BtneCAask84igu34",
+    authDomain: "airsoft-gamemaster-tool.firebaseapp.com",
+    projectId: "airsoft-gamemaster-tool",
+    storageBucket: "airsoft-gamemaster-tool.appspot.com",
+    messagingSenderId: "651326007639",
+    appId: "1:651326007639:web:50975fdc4e337c7a5d7612",
+    measurementId: "G-F8F1DBRS6S"
+};
+
+
