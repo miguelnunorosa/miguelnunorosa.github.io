823fd75b27550178f87252aa5d0179f5b5786da1
Miguel Rosa <46021063+miguelnunorosa@users.noreply.github.com>
2024-08-02 17:01:11 +0000

    update

--- projects/airsoft/api/game1x1.js
+++ projects/airsoft/api/game1x1.js
@@ -1,20 +1,9 @@
-// Configuração do Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyCyj-d_dhgMXHqKkG0BtneCAask84igu34",
-    authDomain: "airsoft-gamemaster-tool.firebaseapp.com",
-    projectId: "airsoft-gamemaster-tool",
-    storageBucket: "airsoft-gamemaster-tool.appspot.com",
-    messagingSenderId: "651326007639",
-    appId: "1:651326007639:web:50975fdc4e337c7a5d7612",
-    measurementId: "G-F8F1DBRS6S"
-};
-
-
-
 
 // Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+
 
 // Função para buscar jogadores da coleção "players" e preencher os dropdowns
 async function fetchPlayers() {
@@ -48,6 +37,8 @@ async function fetchPlayers() {
         }
     });
 }
+
+
 
 // Função para incrementar pontuações
 async function submitResults(event) {
