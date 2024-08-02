e46400068946017e26467af2bace358d0b766140
Miguel Rosa <46021063+miguelnunorosa@users.noreply.github.com>
2024-08-02 18:10:02 +0000

    show results table

--- projects/airsoft/api/game1x1.js
+++ projects/airsoft/api/game1x1.js
@@ -146,3 +146,4 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('game-form').addEventListener('submit', submitResults);
     loadResults(); // Carregar resultados na inicialização
 });
+
