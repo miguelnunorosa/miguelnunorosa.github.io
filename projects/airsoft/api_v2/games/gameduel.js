6c2e5fb4a3140d330b5c94fe98eadc426a572535
Miguel Rosa <46021063+miguelnunorosa@users.noreply.github.com>
2024-08-04 18:25:32 +0000

    add new version: api v2

---
+++ projects/airsoft/api_v2/games/gameduel.js
@@ -0,0 +1,156 @@
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCyj-d_dhgMXHqKkG0BtneCAask84igu34",
+    authDomain: "airsoft-gamemaster-tool.firebaseapp.com",
+    projectId: "airsoft-gamemaster-tool",
+    storageBucket: "airsoft-gamemaster-tool.appspot.com",
+    messagingSenderId: "651326007639",
+    appId: "1:651326007639:web:50975fdc4e337c7a5d7612",
+    measurementId: "G-F8F1DBRS6S"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// Função para buscar jogadores da coleção "players"
+async function fetchPlayerNames() {
+    const players = [];
+    try {
+        const snapshot = await db.collection('players').get();
+        snapshot.forEach(doc => {
+            players.push(doc.data().playerName);
+        });
+    } catch (error) {
+        console.error('Erro ao buscar nomes de jogadores: ', error);
+    }
+    return players;
+}
+
+// Função para preencher os dropdowns
+async function populateDropdowns() {
+    const playerNames = await fetchPlayerNames();
+    
+    const player1Select = document.getElementById('player1Name');
+    const player2Select = document.getElementById('player2Name');
+
+    player1Select.innerHTML = '<option value="" disabled selected>Selecione um jogador</option>';
+    player2Select.innerHTML = '<option value="" disabled selected>Selecione um jogador</option>';
+
+    playerNames.forEach(name => {
+        const option1 = document.createElement('option');
+        option1.value = name;
+        option1.textContent = name;
+        player1Select.appendChild(option1);
+
+        const option2 = document.createElement('option');
+        option2.value = name;
+        option2.textContent = name;
+        player2Select.appendChild(option2);
+    });
+
+    player1Select.addEventListener('change', () => updateDropdowns(player1Select, player2Select));
+    player2Select.addEventListener('change', () => updateDropdowns(player2Select, player1Select));
+}
+
+// Função para desativar jogador selecionado no outro dropdown
+function updateDropdowns(changedSelect, otherSelect) {
+    const selectedValue = changedSelect.value;
+
+    Array.from(otherSelect.options).forEach(option => {
+        if (option.value === selectedValue) {
+            option.disabled = true;
+        } else {
+            option.disabled = false;
+        }
+    });
+}
+
+// Função para carregar Lista Jogadores
+async function loadGameResults() {
+    console.log('Carregando resultados dos jogos...');
+    try {
+        const game1x1Results = document.getElementById('game-1x1-table-body');
+        game1x1Results.innerHTML = ''; // Limpar a tabela antes de adicionar novos dados
+
+        const snapshot = await db.collection('game-1x1-results').orderBy('timestamp', 'desc').get();
+        snapshot.forEach(doc => {
+            const data = doc.data();
+            const player1Name = data.player1Name;
+            const player1Score = data.player1Score;
+            const player2Name = data.player2Name;
+            const player2Score = data.player2Score;
+            const timestamp = data.timestamp ? new Date(data.timestamp.seconds * 1000).toLocaleString() : 'Data não disponível';
+
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${player1Name}</td>
+                <td>${player1Score}</td>
+                <td>${player2Score}</td>
+                <td>${player2Name}</td>
+                <td>${timestamp}</td>
+            `;
+            game1x1Results.append(row);
+        });
+
+        // Destruir a DataTable existente antes de reinicializá-la
+        if ($.fn.DataTable.isDataTable('#game-1x1-table')) {
+            $('#game-1x1-table').DataTable().destroy();
+        }
+
+        // Inicializa a DataTable após os dados serem carregados
+        $('#game-1x1-table').DataTable();
+        console.log('Resultados dos jogos carregados com sucesso.');
+    } catch (error) {
+        console.error('Erro ao carregar Lista de Jogos: ', error);
+    }
+}
+
+// Função para adicionar um novo resultado
+async function addResult(player1Name, player1Score, player2Name, player2Score) {
+    try {
+        await db.collection('game-1x1-results').add({
+            player1Name: player1Name,
+            player1Score: player1Score,
+            player2Name: player2Name,
+            player2Score: player2Score,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        });
+        console.log('Resultado adicionado com sucesso');
+        //
+    } catch (error) {
+        console.error('Erro ao adicionar Resultado: ', error);
+    }
+}
+
+// Função para lidar com o envio do formulário de adicionar resultado
+async function handleAddResultFormSubmit(event) {
+    event.preventDefault();
+    console.log('Enviando formulário para adicionar resultado...');
+
+    const player1Name = document.getElementById('player1Name').value;
+    const player1Score = parseInt(document.getElementById('player1Score').value);
+    const player2Name = document.getElementById('player2Name').value;
+    const player2Score = parseInt(document.getElementById('player2Score').value);
+
+    if (player1Name && player2Name && !isNaN(player1Score) && !isNaN(player2Score)) {
+        await addResult(player1Name, player1Score, player2Name, player2Score);
+
+        // Fechar o modal
+        $('#addGameModal').modal('hide');
+        
+        // Resetar o formulário
+        document.getElementById('add-game-form').reset();
+        console.log('Formulário enviado e modal fechado.');
+        await loadGameResults();
+    } else {
+        console.error('Erro ao adicionar Resultado: Verifique os dados inseridos.');
+    }
+}
+
+// Inicializa a página
+document.addEventListener('DOMContentLoaded', () => {
+    populateDropdowns();
+    document.getElementById('add-game-form').addEventListener('submit', handleAddResultFormSubmit);
+    loadGameResults();
+});
