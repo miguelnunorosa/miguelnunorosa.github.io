54c654bd76c8b3975c7117dfdb8921c855e45bb1
Miguel Rosa <46021063+miguelnunorosa@users.noreply.github.com>
2024-09-14 23:12:36 +0000

    add banner

---
+++ projects/airsoft/pages/teste/content/main-banner.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const content = document.getElementById('content');
+
+    const pageContent = `
+        <div class="main-banner" id="top">
+            <!--<video autoplay muted loop id="bg-video">
+                <source src="assets/images/gym-video.mp4" type="video/mp4" />
+            </video>-->
+            <img src="assets/images/logo_team.png" id="logo-team-first-page">
+        </div>
+    `;
+
+    content.innerHTML = pageContent;
+});
+
+
