3d06fb4289c8c4c62e0cb0e6aeb48c12688ace6c
Miguel Rosa <46021063+miguelnunorosa@users.noreply.github.com>
2024-09-14 20:32:45 +0000

    update

---
+++ projects/airsoft/pages/teste/content/header.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const content = document.getElementById('content');
+
+    const pageContent = `
+       <header class="header-area header-sticky">
+        <div class="container">
+        <div class="row">
+            <div class="col-12">
+            <nav class="main-nav">
+                <!-- ***** Logo Start ***** -->
+                <a href="index.html" class="logo">Wild Ducks <em> Airsoft Team</em></a>
+                <!-- ***** Logo End ***** -->
+                <!-- ***** Menu Start ***** -->
+                <ul class="nav">
+                <li class="scroll-to-section"><a href="#top">Home</a></li>
+                <li class="scroll-to-section"><a href="#features">Quem Somos</a></li>
+                <li class="scroll-to-section"><a href="#our-classes">Equipa</a></li>
+                <li class="scroll-to-section"><a href="#contact-us">Contacto</a></li>
+                <li class="scroll-to-section"><a href="#"></a></li>
+                </ul>
+                <a class='menu-trigger'>
+                <span>Menu</span>
+                </a>
+                <!-- ***** Menu End ***** -->
+            </nav>
+            </div>
+        </div>
+        </div>
+    </header>
+    `;
+
+    content.innerHTML = pageContent;
+});
